9f8e91f8f5f6dd2d907a7acba7e2cb20c074b52d
just4jin <just4jin@users.noreply.github.com>
2014-12-04 12:18:45 -0500

    Update discriminants--trees.R

--- discriminants--trees.R
+++ discriminants--trees.R
@@ -1,15 +1,17 @@
 #**************************************************************************************
 #
-#  		Linear Models, Discriminants,  Additive Models and trees
+#    	Linear Models, Discriminants, Additive Models and trees
 #
 #**************************************************************************************
+
+# Data Cleaning
 
 # Code for evaluation 
 source("ROC.R")
 
 # Training & Test data sets
-bank_train<-read.table("bank-full.csv",sep=";",head=TRUE)
-bank_test<-read.table("bank.csv",sep=";",head=TRUE)
+bank_train<-read.table("bank-full.csv",sep=";",head=T)
+bank_test<-read.table("bank.csv",sep=";",head=T)
 summary(bank_train)
 
 # Comparing quantitative variables for 2 data sets
@@ -20,10 +22,10 @@ for(i in c(1,6,10,12:15)){
   axis(1,at=1:2,labels=c("Original/Train","Test"),tick=TRUE)
 } 
 
-# For discriminant analysis we need to remove the categorical predictors: 
+# For discriminant analysis remove the categorical predictors: 
 # 2-job, 3-marital, 4-education, 5-default, 7-housing, 8-loan, 9-contact, 11-month, 16-outcome
-bank_trainDiscriminants<-bank_train[,-c(2,3,4,5,7,8,9,11,16)]
-bank_testDiscriminants<-bank_test[,-c(2,3,4,5,7,8,9,11,16)]
+bank_train_discri<-bank_train[,-c(2,3,4,5,7,8,9,11,16)]
+bank_test_discri<-bank_test[,-c(2,3,4,5,7,8,9,11,16)]
 
 #**************************************************************************************
 #
@@ -33,23 +35,23 @@ bank_testDiscriminants<-bank_test[,-c(2,3,4,5,7,8,9,11,16)]
 library(MASS)
 
 # Linear discriminant regression
-train_lda <- lda(y~., data = bank_trainDiscriminants)
+train_lda <- lda(y~., data = bank_train_discri)
 
 # Training set fit
 fit_lda <- predict(train_lda)
 # Confusion matrix for training set (uses ROC code)
-score.table(fit_lda$posterior[,2], bank_trainDiscriminants$y)
+score.table(fit_lda$posterior[,2], bank_train_discri$y)
 # Plot of training set performance
 par(mfrow=c(1,1))
-plot(fit_lda$posterior[,2], bank_trainDiscriminants$y, pch = 21, main = "LDA Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_lda$posterior[,2], bank_train_discri$y, pch = 21, main = "LDA Results for Bank Train Data", xlab = "Posterior", ylab = "Actual" )
 
 # Test set fit
-pred_lda <- predict(train_lda, newdata = bank_testDiscriminants)
+pred_lda <- predict(train_lda, newdata = bank_test_discri)
 # Confusion matrix for test set
-score.table(pred_lda$posterior[,2], bank_testDiscriminants$y)
+score.table(pred_lda$posterior[,2], bank_test_discri$y)
 # Plot of test set performance
 par(mfrow=c(1,1))
-plot(pred_lda$posterior[,2], bank_testDiscriminants$y, pch = 21, main = "LDA Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
+plot(pred_lda$posterior[,2], bank_test_discri$y, pch = 21, main = "LDA Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 
 #**************************************************************************************
@@ -59,21 +61,21 @@ plot(pred_lda$posterior[,2], bank_testDiscriminants$y, pch = 21, main = "LDA Res
 #**************************************************************************************
 
 # Quadratic discriminant regression
-train_qda <- qda(y~., data = bank_trainDiscriminants)
+train_qda <- qda(y~., data = bank_train_discri)
 
 # Training set fit
 fit_qda <- predict(train_qda)
 # Confusion matrix for training set 
-score.table(fit_qda$posterior[,2], bank_trainDiscriminants$y)
+score.table(fit_qda$posterior[,2], bank_train_discri$y)
 # Plot of training set performance
-plot(fit_qda$posterior[,2], bank_trainDiscriminants$y, pch = 21, main = "QDA Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_qda$posterior[,2], bank_train_discri$y, pch = 21, main = "QDA Results for Bank Train Data", xlab = "Posterior", ylab = "Actual" )
 
 # Test set fit
-pred_qda <- predict(train_qda, newdata = bank_testDiscriminants)
+pred_qda <- predict(train_qda, newdata = bank_test_discri)
 # Confusion matrix for training set
-score.table(pred_qda$posterior[,2], bank_testDiscriminants$y)
+score.table(pred_qda$posterior[,2], bank_test_discri$y)
 # Plot of training set performance
-plot(pred_qda$posterior[,2], bank_testDiscriminants$y, pch = 21, main = "QDA Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
+plot(pred_qda$posterior[,2], bank_test_discri$y, pch = 21, main = "QDA Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 
 #**************************************************************************************
@@ -86,22 +88,22 @@ library(class)
 library("mda")
 
 # mixed model discriminants regression
-train_mda <- mda(y~., data = bank_trainDiscriminants)
+train_mda <- mda(y~., data = bank_train_discri)
 
 # Training set fit
-fit_mda <- predict(train_mda,bank_trainDiscriminants,type = "post")
+fit_mda <- predict(train_mda,bank_train_discri,type = "post")
 # Confusion matrix for training set
-score.table(fit_mda[,2], bank_trainDiscriminants$y)
+score.table(fit_mda[,2], bank_train_discri$y)
 # Plot of training set performance
 par(mfrow=c(1,1))
-plot(fit_mda[,2], bank_trainDiscriminants$y, pch = 21, main = "MDA Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_mda[,2], bank_train_discri$y, pch = 21, main = "MDA Results for Bank Training Data", xlab = "Posterior", ylab = "Actual" )
 
 # Test set fit
-pred_mda <- predict(train_mda,newdata = bank_testDiscriminants,type = "post")
+pred_mda <- predict(train_mda,newdata = bank_test_discri,type = "post")
 # Confusion matrix for test set
-score.table(pred_mda[,2], bank_testDiscriminants$y)
+score.table(pred_mda[,2], bank_test_discri$y)
 # Plot of test set performance
-plot(pred_mda[,2], bank_testDiscriminants$y, pch = 21, main = "MDA Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
+plot(pred_mda[,2], bank_test_discri$y, pch = 21, main = "MDA Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 
 
@@ -121,14 +123,14 @@ fit_glm <- predict(train_glm, type = "response")
 # Confusion matrix for training set
 score.table(fit_glm, bank_train$y)
 # Plot of training set performance
-plot(fit_glm, bank_train$y, pch = 21, main = "GLM Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_glm, bank_train$y, pch = 21, main = "GLM Results for Bank Train Data", xlab = "Posterior", ylab = "Actual" )
 
 # Test set fit
 pred_glm<- predict(train_glm, newdata = bank_test, type = "response")
 # Confusion matrix for training set
 score.table(pred_glm, bank_test$y)
 # Plot of training set performance
-plot(pred_glm, bank_test$y, pch = 21, main = "GLM Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
+plot(pred_glm, bank_test$y, pch = 21, main = "GLM Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 
 #****************************************************************************************
@@ -153,7 +155,7 @@ fit_gam <- predict(train_gam, type = "response")
 # Confusion matrix for training set
 score.table(fit_gam, bank_train$y)
 # Plot of training set performance
-plot(fit_gam, bank_train$y, pch = 21, main = "GAM (witl all variables) Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_gam, bank_train$y, pch = 21, main = "GAM (All variables) Results for Bank Training Data", xlab = "Posterior", ylab = "Actual" )
 
 
 # Test set fit
@@ -161,102 +163,33 @@ pred_gam <- predict(train_gam, newdata = bank_test, type = "response")
 # Confusion matrix for training set
 score.table(pred_gam, bank_test$y)
 # Plot of training set performance
-plot(pred_gam, bank_test$y, pch = 21, main = "GAM (witl all variables) Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
-
+plot(pred_gam, bank_test$y, pch = 21, main = "GAM (All variables) Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 #*****************************************************
 # GAM - Generalized Additive Model step-wise
 #*****************************************************
 
-train_gamStep <- step.gam(train_gam, scope=list("age"=~1+ age +s(age),"job"=~1+job ,"marital"=~1+marital,
-                                                "education"=~1+ education,"default"=~1+ default,"balance"=~1+balance +s(balance),
-                                                "housing"=~1+housing ,"loan"=~1+loan ,"contact"=~1+contact ,
-                                                "day"=~1+day +s(day),"month"=~1+month ,"duration"=~1+duration +s(duration),
-                                                "campaign "=~1+ campaign +s(campaign ),"pdays"=~1+pdays +s(pdays),"previous"=~1+previous +s(previous),
-                                                "poutcome"=~1+ poutcome))
+train_gamStep <- step.gam(train_gam, scope=list("age"=~1+age+s(age),"job"=~1+job ,"marital"=~1+marital,"education"=~1+ education,"default"=~1+ default,"balance"=~1+balance +s(balance),
+"housing"=~1+housing ,"loan"=~1+loan ,"contact"=~1+contact,"day"=~1+day +s(day),"month"=~1+month ,"duration"=~1+duration +s(duration), "campaign "=~1+ campaign +s(campaign ),"pdays"=~1+pdays +s(pdays),"previous"=~1+previous +s(previous), "poutcome"=~1+ poutcome))
 summary(train_gamStep)
+
 # Training set
 fit_gamStep <- predict(train_gamStep, type = "response")
 # Confusion matrix for training set
 score.table(fit_gamStep, bank_train$y)
 # Plot of training set performance
-plot(fit_gamStep, bank_train$y, pch = 21, main = "GAM Stepwise Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
+plot(fit_gamStep, bank_train$y, pch = 21, main = "GAM Stepwise Results for Bank Train Data", xlab = "Posterior", ylab = "Actual" )
 
 # Test set 
 pred_gamStep <- predict(train_gamStep, newdata=bank_test, type = "response")
 # Confusion matrix for training set
 score.table(pred_gamStep, bank_test$y)
 # Plot of training set performance
-plot(pred_gamStep, bank_test$y, pch = 21, main = "GAM Stepwise Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
+plot(pred_gamStep, bank_test$y, pch = 21, main = "GAM Stepwise Results for Bank Test Data", xlab = "Posterior", ylab = "Actual" )
 
 #*********************************************************
 #
-#    	Trees
-#
-#*********************************************************
-
-library("tree")
-
-# Gini
-train_treeGini <- tree(y~., data = bank_train, split = "gini")
-summary(train_treeGini)
-
-# Entropy
-train_treeEntropy <- tree(y~., data = bank_train,split = "deviance")
-summary(train_treeEntropy)
-
-
-#plot the trees
-plot(train_treeGini,main="Gini Tree")
-text(train_treeGini)
-par(mfrow = c(1,1))
-plot(train_treeEntropy,main="Entropy Tree")
-text(train_treeEntropy)
-
-# entropy tree misclassification rate  #4 is good enough
-plot(cv.tree(train_treeEntropy,FUN=prune.tree,method="misclass"),main="entropy tree missclassification rate")
-default is impurity in terms of deviance/cross-entropy  # or 7 is the best
-plot(cv.tree(train_treeEntropy),main="Entropy tree CV error")
-
-#gini tree misclassification rate  
-plot(cv.tree(train_treeGini,,FUN=prune.tree,method="misclass")) #no leave is the best?
-default is impurity in terms of deviance/cross-entropy  
-plot(cv.tree(train_treeGini,,FUN=prune.tree),main="Gini tree CV error ")
-
-prune the entropy tree (leaves=4)
-pruneTreeEntropy1<-prune.tree(train_treeEntropy,best=4)
-plot(pruneTreeEntropy1,main="pruned entropy tree(leaves=4)")
-text(pruneTreeEntropy1)
-
-prune the entropy tree (leaves=7)
-pruneTreeEntropy2<-prune.tree(train_treeEntropy,best=7)
-plot(pruneTreeEntropy2,,main="pruned entropy tree(leaves=7)")
-text(pruneTreeEntropy2)
-
-prediction on test set(Entropy leaves=4)
-pred_treeEntropy_L4<-predict(pruneTreeEntropy1,newdata=bank_test,type="class")
-table(pred_treeEntropy_L4,bank_test$y)
-
-prediction on test set(Entropy leaves=7)
-pred_treeEntropy_L7<-predict(pruneTreeEntropy2,newdata=bank_test,type="class")
-table(pred_treeEntropy_L7,bank_test$y)
-
-
-
-prune the gini tree (leaves=4)
-pruneTreeGini<-prune.tree(train_treeGini,best=4)
-plot(pruneTreeGini,main="pruned gini tree(leaf?)")
-text(pruneTreeGini)
-
-
-prediction on test set(Entropy leaves=4)
-pred_treeGini_best<-predict(pruneTreeGini,newdata=bank_test,type="class")
-table(pred_treeGini_best,bank_test$y)
-
-
-#*********************************************************
-#
-#    	Trees using rpart
+#    	Trees (rpart)
 #
 #*********************************************************
 
@@ -308,11 +241,10 @@ summary(rparti_pred)
 score.table(rpart_pred[,2], bank_test$y)
 score.table(rparti_pred[,2], bank_test$y)
 
-# Just trees
+# Tree comparison
 plot.roc(rpart_pred[,2], bank_test$y)
 lines.roc(rparti_pred[,2], bank_test$y, col = "red2")
 legend(.5, .25, legend = c("gini tree", "entropy tree"), lwd = 2, col = c("blue", "red2"))
-
 
 #*********************************************************
 #
@@ -320,14 +252,14 @@ legend(.5, .25, legend = c("gini tree", "entropy tree"), lwd = 2, col = c("blue"
 #
 #*********************************************************
 
-# Just Discriminants
+# Discriminant Models Comparison
 plot.roc(pred_lda$post[,2], bank_test$y)
-lines.roc(pred_qda$post[,2], bank_test$y, col = "red2")
-lines.roc(pred_mda[,2], bank_test$y, col = "green")
-legend(.7, .3, legend = c("LDA", "QDA", "MDA"), lwd = 2, col = c("blue", "red2", "green"))
+lines.roc(pred_qda$post[,2], bank_test$y, col = "black")
+lines.roc(pred_mda[,2], bank_test$y, col = "grey")
+legend(.6, .4, legend = c("LDA", "QDA", "MDA"), lwd = 2, col = c("blue", "black", "grey"))
 
 # GLM and GAM
 plot.roc(pred_glm, bank_test$y)
-lines.roc(pred_gam, bank_test$y, col = "green")
-lines.roc(pred_gamStep,bank_test$y, col = "purple")
-legend(.7, .3, legend = c("GLM",  "GAM", "GAM Step"), lwd = 2, col = c("blue",  "green", "purple"))
+lines.roc(pred_gam, bank_test$y, col = "yellow")
+lines.roc(pred_gamStep,bank_test$y, col = "red")
+legend(.6, .4, legend = c("GLM",  "GAM", "GAM Step"), lwd = 2, col = c("blue",  "yellow", "red"))
