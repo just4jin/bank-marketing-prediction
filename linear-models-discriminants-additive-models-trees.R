99bb12692160098ab0883e416e50a343fdc3620a
just4jin <just4jin@users.noreply.github.com>
2014-12-04 11:38:44 -0500

    Update linear-models-discriminants-additive-models-trees.R

--- linear-models-discriminants-additive-models-trees.R
+++ linear-models-discriminants-additive-models-trees.R
@@ -12,7 +12,6 @@ bank_train<-read.table("bank-full.csv",sep=";",head=TRUE)
 bank_test<-read.table("bank.csv",sep=";",head=TRUE)
 summary(bank_train)
 
-
 # Comparing quantitative variables for 2 data sets
 par(mfrow=c(3,3))
 for(i in c(1,6,10,12:15)){
@@ -21,27 +20,20 @@ for(i in c(1,6,10,12:15)){
   axis(1,at=1:2,labels=c("Original/Train","Test"),tick=TRUE)
 } 
 
-
-
-
 # For discriminant analysis we need to remove the categorical predictors: 
 # 2-job, 3-marital, 4-education, 5-default, 7-housing, 8-loan, 9-contact, 11-month, 16-outcome
 bank_trainDiscriminants<-bank_train[,-c(2,3,4,5,7,8,9,11,16)]
 bank_testDiscriminants<-bank_test[,-c(2,3,4,5,7,8,9,11,16)]
-
-
 
 #**************************************************************************************
 #
 #  		Linear Discriminants
 #
 #**************************************************************************************
-
 library(MASS)
 
 # Linear discriminant regression
 train_lda <- lda(y~., data = bank_trainDiscriminants)
-
 
 # Training set fit
 fit_lda <- predict(train_lda)
@@ -51,7 +43,6 @@ score.table(fit_lda$posterior[,2], bank_trainDiscriminants$y)
 par(mfrow=c(1,1))
 plot(fit_lda$posterior[,2], bank_trainDiscriminants$y, pch = 21, main = "LDA Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
 
-
 # Test set fit
 pred_lda <- predict(train_lda, newdata = bank_testDiscriminants)
 # Confusion matrix for test set
@@ -59,8 +50,6 @@ score.table(pred_lda$posterior[,2], bank_testDiscriminants$y)
 # Plot of test set performance
 par(mfrow=c(1,1))
 plot(pred_lda$posterior[,2], bank_testDiscriminants$y, pch = 21, main = "LDA Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
-
-
 
 
 #**************************************************************************************
@@ -87,8 +76,6 @@ score.table(pred_qda$posterior[,2], bank_testDiscriminants$y)
 plot(pred_qda$posterior[,2], bank_testDiscriminants$y, pch = 21, main = "QDA Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
 
 
-
-
 #**************************************************************************************
 #
 #      Mixture Model Discriminants
@@ -101,7 +88,6 @@ library("mda")
 # mixed model discriminants regression
 train_mda <- mda(y~., data = bank_trainDiscriminants)
 
-
 # Training set fit
 fit_mda <- predict(train_mda,bank_trainDiscriminants,type = "post")
 # Confusion matrix for training set
@@ -109,7 +95,6 @@ score.table(fit_mda[,2], bank_trainDiscriminants$y)
 # Plot of training set performance
 par(mfrow=c(1,1))
 plot(fit_mda[,2], bank_trainDiscriminants$y, pch = 21, main = "MDA Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
-
 
 # Test set fit
 pred_mda <- predict(train_mda,newdata = bank_testDiscriminants,type = "post")
@@ -138,7 +123,6 @@ score.table(fit_glm, bank_train$y)
 # Plot of training set performance
 plot(fit_glm, bank_train$y, pch = 21, main = "GLM Results for Bank Data (train)", xlab = "Posterior", ylab = "Actual" )
 
-
 # Test set fit
 pred_glm<- predict(train_glm, newdata = bank_test, type = "response")
 # Confusion matrix for training set
@@ -147,20 +131,17 @@ score.table(pred_glm, bank_test$y)
 plot(pred_glm, bank_test$y, pch = 21, main = "GLM Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
 
 
-
-
 #****************************************************************************************
 #
 #    GAM - Generalized Additive Models
 #
 #****************************************************************************************
 
-library("gam")
-
-
 #*****************************************************
 # GAM - Generalized Additive Model with all variables
 #*****************************************************
+
+library("gam")
 
 # GAM on all quantitative and qualitative variables
 train_gam<-gam(y~.,data=bank_train,family="binomial")
@@ -208,48 +189,44 @@ score.table(pred_gamStep, bank_test$y)
 # Plot of training set performance
 plot(pred_gamStep, bank_test$y, pch = 21, main = "GAM Stepwise Results for Bank Data (test)", xlab = "Posterior", ylab = "Actual" )
 
-
-
-
 #*********************************************************
 #
 #    	Trees
 #
 #*********************************************************
 
-#library("tree")
+library("tree")
 
 # Gini
-#train_treeGini <- tree(y~., data = bank_train, split = "gini")
-#summary(train_treeGini)
+train_treeGini <- tree(y~., data = bank_train, split = "gini")
+summary(train_treeGini)
 
 # Entropy
-#train_treeEntropy <- tree(y~., data = bank_train,split = "deviance")
-#summary(train_treeEntropy)
+train_treeEntropy <- tree(y~., data = bank_train,split = "deviance")
+summary(train_treeEntropy)
 
 
 #plot the trees
-#plot(train_treeGini,main="Gini Tree")
-#text(train_treeGini)
-#par(mfrow = c(1,1))
-#plot(train_treeEntropy,main="Entropy Tree")
-#text(train_treeEntropy)
+plot(train_treeGini,main="Gini Tree")
+text(train_treeGini)
+par(mfrow = c(1,1))
+plot(train_treeEntropy,main="Entropy Tree")
+text(train_treeEntropy)
 
 # entropy tree misclassification rate  #4 is good enough
-#plot(cv.tree(train_treeEntropy,FUN=prune.tree,method="misclass"),main="entropy tree missclassification rate")
-#default is impurity in terms of deviance/cross-entropy  # or 7 is the best
-#plot(cv.tree(train_treeEntropy),main="Entropy tree CV error")
+plot(cv.tree(train_treeEntropy,FUN=prune.tree,method="misclass"),main="entropy tree missclassification rate")
+default is impurity in terms of deviance/cross-entropy  # or 7 is the best
+plot(cv.tree(train_treeEntropy),main="Entropy tree CV error")
 
-# gini tree misclassification rate  
-#plot(cv.tree(train_treeGini,,FUN=prune.tree,method="misclass")) #no leave is the best?
-#default is impurity in terms of deviance/cross-entropy  
-#plot(cv.tree(train_treeGini,,FUN=prune.tree),main="Gini tree CV error ")
+#gini tree misclassification rate  
+plot(cv.tree(train_treeGini,,FUN=prune.tree,method="misclass")) #no leave is the best?
+default is impurity in terms of deviance/cross-entropy  
+plot(cv.tree(train_treeGini,,FUN=prune.tree),main="Gini tree CV error ")
 
-
-#prune the entropy tree (leaves=4)
-#pruneTreeEntropy1<-prune.tree(train_treeEntropy,best=4)
-#plot(pruneTreeEntropy1,main="pruned entropy tree(leaves=4)")
-#text(pruneTreeEntropy1)
+prune the entropy tree (leaves=4)
+pruneTreeEntropy1<-prune.tree(train_treeEntropy,best=4)
+plot(pruneTreeEntropy1,main="pruned entropy tree(leaves=4)")
+text(pruneTreeEntropy1)
 
 prune the entropy tree (leaves=7)
 pruneTreeEntropy2<-prune.tree(train_treeEntropy,best=7)
@@ -285,9 +262,6 @@ table(pred_treeGini_best,bank_test$y)
 
 library("rpart")
 
-
-
-
 # Rpart tree
 # Gini
 train_rpart <- rpart(y~., data = bank_train, method = "class")
@@ -309,14 +283,12 @@ text(train_rparti,use.n=TRUE,pretty=3)
 plotcp(train_rpart)
 plotcp(train_rparti)
 
-
 # Selecting the best tree by cv error
 best.rpart <- function(rpart.obj)
 {
   cp <- rpart.obj$cptable[which.min(rpart.obj$cptable[,4]),1]
   prune.rpart(rpart.obj, cp = cp)
 }
-
 
 train_rpart_best<- best.rpart(train_rpart) 
 plot(train_rpart_best,main="pruned Gini tree train")
@@ -348,13 +320,11 @@ legend(.5, .25, legend = c("gini tree", "entropy tree"), lwd = 2, col = c("blue"
 #
 #*********************************************************
 
-
 # Just Discriminants
 plot.roc(pred_lda$post[,2], bank_test$y)
 lines.roc(pred_qda$post[,2], bank_test$y, col = "red2")
 lines.roc(pred_mda[,2], bank_test$y, col = "green")
 legend(.7, .3, legend = c("LDA", "QDA", "MDA"), lwd = 2, col = c("blue", "red2", "green"))
-
 
 # GLM and GAM
 plot.roc(pred_glm, bank_test$y)
